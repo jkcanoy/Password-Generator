3054f77dbf67880270daa9d14ef86982e41c03b2
Kyle Canoy <kcanoy@uw.edu>
2021-04-08 13:22:17 -0700

    added confirmation prompts

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var charSet = {
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: "!@#$%^&*()-_+=[{]}\|;:,<.>/?"
+};
+
+var passlength = prompt("How long would you like your password? Enter a number between 8 and 128.")
+var lowerCase = confirm("Would you like this password to contain lowercase letters?")
+var upperCase = confirm("Would you like this password to contain uppercase letters?")
+var number = confirm("Would you like this password to contain numbers?")
+var symbol = confirm("Would you like this password to special characters?")
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,15 +25,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var charSet = {
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "0123456789",
-  special: "!@#$%^&*()-_+=[{]}\|;:,<.>/?"
-};
-
-var length = window.prompt
