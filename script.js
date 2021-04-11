146b68c4b473cedba08282fd5eaf5d3a5585f4b7
Kyle Canoy <kcanoy@uw.edu>
2021-04-11 12:59:05 -0700

    converted charSet properties to arrays

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ generateBtn.addEventListener("click", writePassword);
 
   // Password character set
   var charSet = {
-    lower: "abcdefghijklmnopqrstuvwxyz",
-    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numeric: "0123456789",
-    special: "!@#$%^&*()-_+=[{]}|;:,<.>/?"
+    lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    upper: lower.toUpperCase(),
+    numeric: ["0","1","2","3","4","5","6","7","8","9"],
+    special: ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","[","{","]","}","|",";",":",",","<",".",">","/","?"]
   };  
 
 // Variables
